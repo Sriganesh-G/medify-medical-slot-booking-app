e0594e949ab15dbd17b9dd34e48a9f84164f0324
Sriganesh-G <sriganesh9647@gmail.com>
2024-09-20 16:51:18 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home.jsx";
+import SearchResultsPage from "./pages/SearchResultsPage/SearchResultsPage.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "search", element: <SearchResultsPage /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchResultsPage = () => {
+  return <div>SearchResultsPage</div>;
+};
+
+export default SearchResultsPage;
